37ec18a4328c4a5124bd8bb9d89af567002f17d4
Ahmad-mustapha <ahmadishola12@gmail.com>
2025-02-10 21:15:59 +0100

    Adding necessary components

---
+++ src/components/import.js
@@ -0,0 +1,5 @@
+export { default as Login } from '../components/login/Login'
+export { default as Hero } from '../components/hero/Hero'
+export { default as Footer } from '../components/footer/Footer'
+export { default as Contact } from '../components/contactUs/Contact'
+
